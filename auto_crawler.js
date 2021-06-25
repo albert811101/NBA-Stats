4e4035ad2feb1e6ded6a338d5359ab68871ff22d
Albert Lin <albert_811101@hotmail.com>
2021-06-25 17:17:13 +0800

    fix cron

--- auto_crawler.js
+++ auto_crawler.js
@@ -1,16 +1,16 @@
 const cron = require("node-cron");
 const { fetchBoxscore, createPlayerStats } = require("./server/controllers/fantasy_controller");
 
-cron.schedule("30 16 * * *", () => {
+cron.schedule("00 09 * * *", () => {
   console.log("add today boxscore");
   fetchBoxscore();
 });
 
-cron.schedule("31 16 * * *", () => {
+cron.schedule("22 09 * * *", () => {
   console.log("update player stats in playoff");
   createPlayerStats();
 });
 
-cron.schedule("32 16 * * *", () => {
+cron.schedule("23 09 * * *", () => {
   console.log("good job!");
 });
